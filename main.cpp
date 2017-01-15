c5b67dbb1bc66b23ec25ff9a4ec4c3435a166051
lydia <lydiatatchell@gmail.com>
2017-01-15 15:58:19 +0000

    no i have no idea any more

--- main.cpp
+++ main.cpp
@@ -30,6 +30,24 @@ using namespace std;
                                     sdens[N - 2]*sqrt(radii[N - 2]))/
                 (radii[N - 1] - radii[N - 2]);
 
+        /*lambda = rate of angular momentum per unit mass by tidal interaction
+         * = sgn(R - Rj)*((0.23((Mj/M)^2)*G*M)/2R)*(R/|R-Rj|)*/
+        double lambda;
+        int Mj =  1.898e27; //mass of jupiter like planet
+        int M = 1.99e29;
+        int Rj = 7.786e8;
+        int k = radii[0] - Rj;
+        double g = 6.67e-11;
+
+        vector sgn (double k) {
+            if (k<0) return -1;
+
+            if (k>0) return 1; //if k is less than 0, return -1. if not, return 0 or -1
+        }
+        lambda = (sgn(k)*((0.23*((Mj/M)^2)*g*M)/2*radii[0]))*((radii[0])/(radii[0] - Rj));
+        cout << lambda << endl;
+
+
 
         // fdens = (3*visc/R)*d/dr(temp)
         fdens[0] = (3.0*visc/radii[0])*(temp[1] - temp[0])/(radii[1] - radii[0]);
